0f065bd98549baed3bab1686e65586bc9a087531
ChrisWittman24 <chriswittman24@gmail.com>
2025-04-04 10:38:50 -0500

    SelectItem(Using numpad)

--- DC231/Assets/Scripts/InventoryManager.cs
+++ DC231/Assets/Scripts/InventoryManager.cs
@@ -6,7 +6,37 @@ public class InventoryManager : MonoBehaviour
 {
     public InventorySlot[] inventorySlots;
     public GameObject inventoryItemPrefab;
-    public void AddItem(Item item)
+
+    int selectedGameSlot = -1;
+
+    private void Start()
+    {
+        ChangeSelectedSlot(0);
+    }
+
+    private void Update()
+    {
+        if (Input.inputString != null)
+        {
+            bool isNumber = int.TryParse(Input.inputString, out int number);
+            if (isNumber && number > 0 && number < 6)
+            {
+                ChangeSelectedSlot(number - 1);
+            }
+        }
+    }
+
+    void ChangeSelectedSlot(int newValue)
+    {
+        if (selectedGameSlot >= 0)
+        {
+            inventorySlots[selectedGameSlot].Deselect();
+        }
+        inventorySlots[newValue].Select();
+        selectedGameSlot = newValue;
+    }
+
+    public bool AddItem(Item item)
     {
         for (int i = 0; i < inventorySlots.Length; i++)
         {
@@ -15,9 +45,11 @@ public class InventoryManager : MonoBehaviour
             if (itemInSlot == null)
             {
                 SpawnNewItem(item, slot);
-                return;
+                return true; //Item added successfully
             }
         }
+
+        return false; //Inventory is full
     }
 
     void SpawnNewItem(Item item, InventorySlot slot)
@@ -25,5 +57,16 @@ public class InventoryManager : MonoBehaviour
         GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
         InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
         inventoryItem.InitialiseItem(item);
+    }
+
+    public Item GetSelectedItem()
+    {
+        InventorySlot slot = inventorySlots[selectedGameSlot];
+        InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+        if (itemInSlot != null)
+        {
+            return itemInSlot.item;
+        }
+        return null; // No item in the selected slot
     }
 }
