153c41c72d7ae07efec356c4d3b2e0250b4f4e47
Bimbino <baileyshollenberger@gmail.com>
2025-04-30 23:59:22 -0500

    Redundant Data Cleanup
    I deleted the old scripts and custom object type that I created to handle the level-specific sprites.

--- DC231/Assets/Scripts/SpriteSetData.cs
+++
@@ -1,9 +0,0 @@
-//This acts to hold the sprites for each prefab per theme
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "SpriteSet", menuName = "Custom/Sprite Set")]
-public class SpriteSetData : ScriptableObject
-{
-    public List<Sprite> sprites = new List<Sprite>();
-}
